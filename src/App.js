8cf5cedee7717ca5e5ea6c3f36c683d29046a15e
szotaksimon <szotak.simon16@gmail.com>
2022-10-05 21:14:51 +0200

    Add Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import MainNavbar from "./components/navigation/Navbar";
+import Card from "./components/cards/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import classes from "./styles/app/app.module.scss"
 
 function App() {
   return (
     <>
       <MainNavbar />
+      <Container className={classes.container}>
+        <Row className={classes.row}>
+          <Col>
+            <Card />
+          </Col>
+          <Col>
+            <Card />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
