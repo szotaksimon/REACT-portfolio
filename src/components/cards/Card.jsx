3450c92ad0860c935e272721170c9abc6f37d619
szotaksimon <szotak.simon16@gmail.com>
2022-10-05 21:15:25 +0200

    Card component létrehozása

---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,11 @@
+import classes from "../../styles/card/card.module.scss";
+
+function Card() {
+  return (
+    <div className={classes.card}>
+      <h1>asd</h1>
+    </div>
+  );
+}
+
+export default Card;
