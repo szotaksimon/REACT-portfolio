d702da219290fc4663e7b2855e859e73ebea25f0
szotaksimon <szotak.simon16@gmail.com>
2022-10-13 16:59:32 +0200

    Bootstrap navbar lecserélve + sccs

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,26 +1,51 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import classes from '../../styles/navbar/navbar.module.scss'
+import classes from "../../styles/navbar/navbar.module.scss";
+
 
 function MainNavbar() {
   return (
-      <Navbar bg="dark" variant="dark" className={classes.navbar}>
-        <Container>
-          <Navbar.Brand href="#home" className={classes.brand}>My <img
-              alt=""
-              src="logo192.png"
-              width="30"
-              height="30"
-              className={classes.title}
-            />{' '}Portfolio
-            </Navbar.Brand>
-          <Nav>            
-            <Nav.Link className={classes.link} href="https://www.linkedin.com/" target="_blank">LinkedIn</Nav.Link>
-            <Nav.Link className={classes.link} href="https://github.com/szotaksimon/REACT-portfolio" target="_blank">GitHub</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+    <nav className={classes.navbar}>
+      <div className={classes.brand}>
+        My
+        <img alt="React" src="logo192.png" width="40" height="40" className={classes.title} />
+        Portfolio
+      </div>
+      <a href="#" className={classes.toggleB}>
+        <span className={classes.bar}></span>
+        <span className={classes.bar}></span>
+        <span className={classes.bar}></span>
+      </a>
+      <div className={classes.navLinks}>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+
+    // <Navbar bg="dark" variant="dark" classNameName={classes.navbar}>
+    //   <Container>
+    //     <Navbar.Brand href="#home" className={classes.brand}>My <img
+    //         alt=""
+    //         src="logo192.png"
+    //         width="30"
+    //         height="30"
+    //         className={classes.title}
+    //       />{' '}Portfolio
+    //       </Navbar.Brand>
+    //     <Nav>
+    //       <Nav.Link className={classes.link} href="https://www.linkedin.com/" target="_blank">LinkedIn</Nav.Link>
+    //       <Nav.Link className={classes.link} href="https://github.com/szotaksimon/REACT-portfolio" target="_blank">GitHub</Nav.Link>
+    //     </Nav>
+    //   </Container>
+    // </Navbar>
   );
 }
 
