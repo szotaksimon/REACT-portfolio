e2c24fff8802e25cd6b8bbfb01688408ab2db7a7
szotaksimon <szotak.simon16@gmail.com>
2022-10-13 16:22:57 +0200

    + Cards for visual

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import Card from "./components/cards/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import classes from "./styles/app/app.module.scss"
+import classes from "./styles/app/app.module.scss";
 
 function App() {
   return (
     <>
       <MainNavbar />
       <Container className={classes.container}>
+        <Row className={classes.row}>
+          <Col>
+            <Card />
+          </Col>
+          <Col>
+            <Card />
+          </Col>
+        </Row>
+        <Row className={classes.row}>
+          <Col>
+            <Card />
+          </Col>
+          <Col>
+            <Card />
+          </Col>
+        </Row>
         <Row className={classes.row}>
           <Col>
             <Card />
--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -3,8 +3,6 @@ import classes from "../../styles/card/card.module.scss";
 function Card() {
   return (
     <div className={classes.card}>
-      <div className={classes.cover}>
-      </div>
     </div>
   );
 }
